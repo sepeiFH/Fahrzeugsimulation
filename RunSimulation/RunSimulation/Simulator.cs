1785289f4f72da88aa5bc6e160a82d252579ae0d
SebUhb <sunterhitzenberger.itsb-b2013@fh-salzburg.ac.at>
2017-07-13 18:49:39 +0200

    added RunSimulation project to the project folders. This project starts the other ones

---
+++ RunSimulation/RunSimulation/Simulator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace RunSimulation
+{
+
+    class Simulator
+    {
+        public Simulator()
+        {
+            
+        }
+
+        public void RunSimulation()
+        {
+            List<string> Apps = new List<string> { @"..\..\..\..\Simulator\Simulator\bin\Debug\Simulator.exe", @"..\..\..\..\Traffic control\Traffic control\bin\Debug\Traffic control.exe", @"..\..\..\..\FhMapDrawing\FhMapDrawing\bin\Windows\x86\Debug\FhMapDrawing.exe" };
+
+            foreach (string app in Apps)
+            {
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = app;
+                Process.Start(start);
+                int t = app.LastIndexOf(@"\") + 1;
+                string name = app.Substring(t, app.Length - t - 4);
+                Console.WriteLine("Anwendung " + name + " wurde gestartet");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Fahrzeusimulation wurde gestartet");
+            Console.ReadLine();
+        }
+    }
+}
